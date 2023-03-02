36e150e5a05e5d86f841807a90b70bf931adb9bc
pprzemek56 <przemkopis@wp.pl>
2023-03-02 22:17:12 +0100

    first commit

---
+++ src/main/java/system/managment/pharmacy/pharmacy/system/PharmacySystemApplication.java
@@ -0,0 +1,13 @@
+package system.managment.pharmacy.pharmacy.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmacySystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/system/managment/pharmacy/pharmacy/system/PharmacySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package system.managment.pharmacy.pharmacy.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
